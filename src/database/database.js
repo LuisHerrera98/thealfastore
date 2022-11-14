b7e34b5e8917a0df55502b0bb830b4b8155b46c2
luisherrera98 <luchoherrerafernandez@gmail.com>
2022-11-14 10:42:44 -0300

    enviroments loaded

--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,7 @@
 import Sequelize from 'sequelize';
 
 export const sequelize = new Sequelize('induar', 'postgres', 'password', {
-    host: '52.204.121.152',
+    host: '54.156.167.104',
     dialect: 'postgres',
     logging: false
 });
